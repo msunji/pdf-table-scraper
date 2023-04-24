61826368f9e036fb5daa2fb8b398f1d6dbc075b0
Marge <marge.consunji@gmail.com>
2023-04-24 13:09:24 +0800

    Clean data

--- scraper.py
+++ scraper.py
@@ -6,36 +6,27 @@ pdf = plumber.open("./files/April18.pdf")
 p0 = pdf.pages[0]
 cropped = p0.crop((32.07, 140.37, p0.width, 604+140.37))
 
-# im = cropped.to_image()
-# im.debug_tablefinder({
-#     "horizontal_strategy": "text",
-#     "vertical_strategy": "text",
-#     "snap_y_tolerance": 5,
-#     "join_y_tolerance": 2,
-# })
-# im.save("debug.png")
+# Visual debugging
+im = cropped.to_image()
+im.debug_tablefinder({
+    "horizontal_strategy": "text",
+    "vertical_strategy": "text",
+    "snap_y_tolerance": 5,
+    "snap_x_tolerance": 5,
+})
+im.save("debug.png")
 
-# Test tables
+# Extract table
 table = cropped.extract_tables(table_settings={
     "horizontal_strategy": "text",
     "vertical_strategy": "text",
     "snap_y_tolerance": 5,
-    "join_y_tolerance": 2,
+    "snap_x_tolerance": 5,
 })
-print(table)
 
-# Clean data
-# Only get first and last columns
-# print(dfs[0])
-# col_df = dfs[0].loc[:, ["Issue Name Symbol", "Buying/(Selling),"]]
-
-# # Rename columns
-# col_df.columns = ["Stock", "Net Foreign"]
-
-# # Drop ALL rows with at least one NaN value
-# # This is not the same as cells with '-' values
-# # Clears rows we really don't need
-# clean_rows_df = col_df.dropna(axis=0, how="any")
-# # print(clean_rows_df)
+# Clean table
+# Remember to only get the first element in table list
+no_blanks = [row for row in table[0] if not '' in row[1:]]
+print(no_blanks)
 
 # clean_rows_df.to_csv("test.csv")
