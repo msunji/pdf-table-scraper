f2342c7379a9dc4d8cf5ecb746befaac56dd8646
Marge <marge.consunji@gmail.com>
2023-04-26 12:11:24 +0800

    Refactor and organise code

--- scraper.py
+++ scraper.py
@@ -2,9 +2,27 @@ import pdfplumber as plumber
 import pandas as pd
 
 # Open PDF to extract
-pdf = plumber.open("./files/April18.pdf")
+pdf = plumber.open("./files/April25.pdf")
+
+# Visual debugging
+def debug_pdf(page):
+  """
+  Helps figure out if we're extracting the right thing.
+  """
+  img = page.to_image()
+  img.debug_tablefinder({
+    "horizontal_strategy": "text",
+    "vertical_strategy": "text",
+    "snap_y_tolerance": 5,
+    "snap_x_tolerance": 5,
+  })
+  img.save("debug.png")
 
 def extractTables(page):
+    """
+    Extracts tables from each page of the PDF document.
+    Crops pages to only the relevant bits, extracts tables, then returns a list.
+    """
     cropped_page = page.crop((32.07, 165.26, page.width, 604+165.26))
     table = cropped_page.extract_tables(table_settings={
       "horizontal_strategy": "text",
@@ -12,37 +30,39 @@ def extractTables(page):
       "snap_y_tolerance": 5,
       "snap_x_tolerance": 5,
     })
-    # Clean table
-    # Remember to only get the first element in table list
-    # no_blanks should be a list of lists with lists containing strings, numbers and '-' symbols
+
+    """
+    Clean extracted table first. Remember to only get the first element in table list so you're not left with overly nested lists. cleaned_table is a list with lists containing strings, numbers, and '-' symbols.
+
+    In other words, blank rows parsed by extract_tables should be removed accordingly.
+    """
+
     cleaned_table = [row for row in table[0] if not '' in row[1:]]
     return cleaned_table
 
-# Extract tables from pages 1-7 of the report
-# Append all tables to all_tables
-all_tables = []
-for index, page in enumerate(pdf.pages):
-    new_table = extractTables(page)
-    all_tables.extend(new_table)
-    if index == 6:
-        break
-# print(all_tables)
+def scrape_pdfs():
+    """
+    Scrape and compiled tables from pages 1-7 of the PDF document.
+    Returns a list of all scraped and cleaned data.
+    """
+    for index, page in enumerate(pdf.pages):
+      all_tables = []
+      new_table = extractTables(page)
+      all_tables.extend(new_table)
+      if index == 6:
+          break
+    return all_tables
+
+
+pdf_data = scrape_pdfs()
 
 # Convert to pandas dataframe
-df = pd.DataFrame(all_tables)
+df = pd.DataFrame(pdf_data)
 
 # Add column names
 df.columns = ["Stock Name", "Symbol", "Bid", "Ask", "Open", "High", "Low", "Close", "Volume", "Value PHP", "Net Foreign"]
-print(df.head())
+# print(df.head())
 
-df.to_csv("test.csv", index=False)
+# df.to_csv("April24.csv", index=False)
 
-# Visual debugging
-# im = cropped.to_image()
-# im.debug_tablefinder({
-#     "horizontal_strategy": "text",
-#     "vertical_strategy": "text",
-#     "snap_y_tolerance": 5,
-#     "snap_x_tolerance": 5,
-# })
-# im.save("debug.png")
+
