985bcb8a158b55605cd771d83e60058c7961e3a3
Marge <marge.consunji@gmail.com>
2023-04-26 15:29:16 +0800

    Set up Google Sheets to get and update data
    - Get list of stocks in portfolio from gsheets
    - Update net foreign spreadsheet with extracted table data

--- scraper.py
+++ scraper.py
@@ -1,8 +1,29 @@
+import os
+import gspread as gs
 import pdfplumber as plumber
 import pandas as pd
 
-# Open PDF to extract
-pdf = plumber.open("./files/April25.pdf")
+# Work with Gsheets
+credentials = {
+  "type": os.getenv("GAPI_TYPE"),
+  "project_id": os.getenv("PROJECT_ID"),
+  "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+  "private_key": os.getenv("PRIVATE_KEY"),
+  "client_email": os.getenv("CLIENT_EMAIL"),
+  "client_id": os.getenv("CLIENT_ID"),
+  "auth_uri": os.getenv("AUTH_URI"),
+  "token_uri": os.getenv("TOKEN_URI"),
+  "auth_provider_x509_cert_url": os.getenv("AUTH_CERT_URL"),
+  "client_x509_cert_url": os.getenv("CLIENT_CERT_URL")
+}
+
+gc = gs.service_account_from_dict(credentials)
+equity_sh = gc.open("PH Equity Data")
+# Get worksheet
+stocks_ws = equity_sh.worksheet("stocks")
+net_foreign_ws = equity_sh.worksheet("daily_net_foreign")
+# Get stocks list
+stocks_list = stocks_ws.col_values(1)
 
 # Visual debugging
 def debug_pdf(page):
@@ -40,29 +61,43 @@ def extractTables(page):
     cleaned_table = [row for row in table[0] if not '' in row[1:]]
     return cleaned_table
 
-def scrape_pdfs():
+def scrape_pdfs(pdf):
     """
     Scrape and compiled tables from pages 1-7 of the PDF document.
     Returns a list of all scraped and cleaned data.
     """
+    all_tables = []
     for index, page in enumerate(pdf.pages):
-      all_tables = []
       new_table = extractTables(page)
       all_tables.extend(new_table)
       if index == 6:
           break
     return all_tables
 
-
-pdf_data = scrape_pdfs()
+# Open PDF to extract
+pdf = plumber.open("./files/April25.pdf")
+pdf_data = scrape_pdfs(pdf)
 
 # Convert to pandas dataframe
 df = pd.DataFrame(pdf_data)
 
 # Add column names
 df.columns = ["Stock Name", "Symbol", "Bid", "Ask", "Open", "High", "Low", "Close", "Volume", "Value PHP", "Net Foreign"]
-# print(df.head())
 
-# df.to_csv("April24.csv", index=False)
+# Add date column -- date is date when data is collected
+df["Date"] = pd.to_datetime('today').strftime('%m-%d-%Y')
+# Convert () to negative float
+df["Net Foreign"] = (df["Net Foreign"].replace('[(]', '-', regex=True).replace('[),]', '', regex=True).astype(float))
+
+# Filter dataframe to stocks in portfolio
+net_foreign_portfolio = df[df["Symbol"].isin(stocks_list)]
+
+# Rearrange columns
+net_foreign_portfolio = net_foreign_portfolio.reindex(columns=["Stock Name", "Symbol", "Date", "Bid", "Ask", "Open", "High", "Low", "Close", "Volume", "Value PHP", "Net Foreign"])
+
+# Update spreadsheet
+net_foreign_ws.update([net_foreign_portfolio.columns.values.tolist()] + net_foreign_portfolio.values.tolist())
+# Export as CSV
+# portfolio.to_csv("April24.csv", index=False)
 
 
