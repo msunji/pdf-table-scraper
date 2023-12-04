67b11ad5f6973051358999008ec3a0dfdadc149b
Marge <marge.consunji@gmail.com>
2023-12-04 15:30:21 +0800

    Rename file and add functions
    This covers Google Sheets related functions - pulling data from the worksheet in question, getting all records, getting a specific column, pushing data back to Google Sheets.

--- modules/gsheet_actions.py
+++ modules/gsheet_actions.py
@@ -1,4 +1,5 @@
 import gspread as gs
+import os
 
 # GSheets credentials dict
 credentials = {
@@ -13,3 +14,18 @@ credentials = {
   "auth_provider_x509_cert_url": os.getenv("AUTH_CERT_URL"),
   "client_x509_cert_url": os.getenv("CLIENT_CERT_URL")
 }
+
+all_sheets = gs.service_account_from_dict(credentials)
+ph_equity_sh = gc.open("PH Equity Data")
+
+def getWorksheet(worksheet_name):
+  worksheet = equity_sh.worksheet
+  return worksheet
+
+def getAllWSRecords(worksheet_name):
+  all_records = equity_sh.worksheet(worksheet_name).get_all_records()
+  return all_records
+
+def getWSColVals(worksheet_name, col_val):
+  col_values = equity_sh.worksheet(worksheet_name).col_values(col_val)
+  return col_values
